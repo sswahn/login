0ca400e37a774f2d5b7253aaac214e2dd6ca3752
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-13 10:14:32 -0500

    Update ConfirmForgotPassword.js

--- src/components/ConfirmForgotPassword/ConfirmForgotPassword.js
+++ src/components/ConfirmForgotPassword/ConfirmForgotPassword.js
@@ -1,6 +1,65 @@
+import { useState } from 'react'
+import UserIcon from '../Icons/UserIcon.js'
+import PasswordIcon from '../Icons/PasswordIcon.js'
+import SubmitButton from '../SubmitButton/SubmitButton.js'
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner.js'
 
 const ConfirmForgotPassword = ({ className, onSubmit }) => {
+  const [state, setState] = useState({
+    confirmation_code: '',
+    username: '',
+    password: '',
+    confirm_password: ''
+  })
+  const [message, setMessage] = useState(undefined)
+  const [error, setError] = useState(undefined)
+  const [loading, setLoading] = useState(false)
 
+  const handleOnChange = ({ target }) => {
+    setState({ ...state, [target.id]: target.value })
+  }
+
+  const validatePassword = () => {
+    return new Promise((resolve, reject) => {
+      state.password !== state.confirm_password
+        ? reject('Passwords do not match.')
+        : resolve()
+    })
+  }
+
+  const submitRequest = () => {
+    const request = {
+      confirmation_code: state.confirmation_code,
+      username: state.username,
+      password: state.password
+    }
+    return onSubmit(request)
+  }
+
+  const handleResponse = response => {
+    return new Promise((resolve, reject) => {
+      response.error !== undefined
+        ? reject(response.error)
+        : resolve(response.message)
+    })
+  }
+  
+  const handleOnSubmit = async event => {
+    event.preventDefault()
+    try {
+      await validatePassword()
+      setLoading(true) 
+      setError(undefined)
+      const response = await submitRequest()
+      setLoading(false)
+      const data = await handleResponse(response)
+      setMessage(data)
+    } catch (err) {
+      setError(err)
+    }
+  }
+  
+  
   return (
     <form className={`${styles.confirm} ${className || ''}`} onSubmit={handleOnSubmit} aria-label="create new password form">
       <h2>Create a new password</h2>
@@ -19,7 +78,7 @@ const ConfirmForgotPassword = ({ className, onSubmit }) => {
         <input id="confirm_password" type="password" onChange={handleOnChange} required minLength={8} maxLength={130} placeholder="Confirm Password" autoComplete="off" aria-label="confirm password" />
         <PasswordIcon />
       </div>
-      {loading && <Loader />}
+      {loading && <LoadingSpinner />}
       {!loading && <SubmitButton text="Reset Password" message={message} error={error} />}
     </form>
   )
