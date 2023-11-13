a3b199e5c8204bb4979209e3426f5e6740f821aa
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-12 21:10:44 -0500

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,31 @@
+import { useState } from 'react'
+import SubmitButton from '../SubmitButton/SubmitButton.js'
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner.js'
+import styles from './styles.module.css'
 
-const Register = () => {
-
+const Register = ({ className }) => {
+  const [message, setMessage] = useState(undefined)
+  const [error, setError] = useState(undefined)
+  const [loading, setLoading] = useState(false)
+  
   return (
-    <form>
-      <input type="text" />
-      <input type="email" />
-      <input type="password" />
-      <input type="password" />
+    <form className={`${styles.register} ${className || ''}`} onSubmit={handleSubmit} aria-label="registration form">
+      <div>
+        <input type="text" onChange={handleOnChange} placeholder="Username" />
+        <UserIcon />
+      </div>
+      <div>
+        <input type="email" onChange={handleOnChange} placeholder="Email" />
+        <EmailIcon />
+      </div>
+      <div>
+        <input type="password" onChange={handleOnChange} placeholder="Password" />
+        <PasswordIcon />
+      </div>
+      <div>
+        <input type="password" onChange={handleOnChange} placeholder="Confirm Password" />
+        <PasswordIcon />
+      </div>
       <details className="auth-details" aria-label="password requirements">
         <summary>Password requirements</summary>
         <ul>
@@ -17,7 +36,10 @@ const Register = () => {
           <li>Contains at least 1 lowercase letter.</li>
         </ul>
       </details>
-      <button type="submit">Sign up</button>
+      {loading && <LoadingSpinner />}
+      {!loading && <SubmitButton text="Sign Up" message={message} error={error} />}
     </form>
   )
 }
+
+export default Register
