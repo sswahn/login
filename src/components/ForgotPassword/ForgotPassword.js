821112c316e55c54de975540db27c3acb99365c9
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-13 09:52:31 -0500

    Create ForgotPassword.js

---
+++ src/components/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import UserIcon from '../Icons/UserIcon.js'
+
+const ForgotPassword = ({ className, onSubmit }) => {
+  const [username, setUsername] = useState('')
+  const [message, setMessage] = useState(undefined)
+  const [error, setError] = useState(undefined)
+  const [loading, setLoading] = useState(false)
+  
+  const handleOnChange = ({ target }) => {
+    setUsername(target.value)
+  }
+
+  const submitRequest = () => {
+    const request = { username }
+    return onSubmit(request)
+  }
+
+  const handleResponse = response => {
+    return new Promise((resolve, reject) => {
+      response.error !== undefined
+        ? reject(response.error)
+        : resolve(response.message)
+    })
+  }
+  
+  const handleOnSubmit = async () => {
+    try {
+      setLoading(true) 
+      setError(undefined)
+      const response = await submitRequest()
+      setLoading(false)
+      const data = await handleResponse(response)
+      setMessage(data)
+    } catch (err) {
+      setError(err)
+    }
+  }
+  
+  return (
+    <form className={`${styles.forgot} ${className || ''}`} onSubmit={handleOnSubmit} aria-label="forgot password form">
+      <h2>Forgot password</h2>
+      <div>
+        <input id="username" type="text" onChange={handleOnChange} placeholder="Username" required minLength={2} maxLength={50} pattern="^(?!_)[a-zA-Z0-9_]{1,48}(?<!_)$" autoComplete="off" aria-label="username" />
+        <UserIcon />
+      </div>
+      {loading && <Loader />}
+      {!loading && <SubmitButton text="Reset Password" message={message} error={error} />}
+    </form>
+  )
+}
