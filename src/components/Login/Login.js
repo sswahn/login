1e73032cb53a639bf5a3491da58bd2337410f658
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-13 15:34:46 -0500

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -58,11 +58,11 @@ const Login = ({ className, onSubmit, forgotPassword, registerUser }) => {
     <form className={`${styles.login} ${className || ''}`} onSubmit={handleOnSubmit} aria-label="login form">
       <h2>Sign In</h2>
       <div>
-        <input id="username" type="text" onChange={handleOnChange} required minLength={2} maxLength={50} autoComplete="off" aria-label="username" />
+        <input id="username" type="text" onChange={handleOnChange} placeholder="Username" required minLength={2} maxLength={50} autoComplete="off" aria-label="username" />
         <UserIcon />
       </div>
       <div>
-        <input id="password" type="password" onChange={handleOnChange} required minLength={8} maxLength={130} autoComplete="off" aria-label="password" />
+        <input id="password" type="password" onChange={handleOnChange} placeholder="Password" required minLength={8} maxLength={130} autoComplete="off" aria-label="password" />
         <PasswordIcon />
       </div>
       <Checkbox label="Remember me" checked={checked} onChange={handleCheckbox} />
