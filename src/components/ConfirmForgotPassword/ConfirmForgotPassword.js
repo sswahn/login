3922625bff5016b4080ff7fbfae73776c664535b
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-14 21:49:15 -0500

    Update ConfirmForgotPassword.js

--- src/components/ConfirmForgotPassword/ConfirmForgotPassword.js
+++ src/components/ConfirmForgotPassword/ConfirmForgotPassword.js
@@ -45,9 +45,8 @@ const ConfirmForgotPassword = ({ className, onSubmit }) => {
       }
       if (response.error !== undefined) { 
         reject(response.error) 
-      } else {
-        resolve(response.message)
       }
+      resolve(response.message)
     })
   }
 
