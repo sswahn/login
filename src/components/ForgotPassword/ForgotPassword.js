b57616ad41185b209efa7ff6407976e6d9c048a0
S. Swahn <36422765+sswahn@users.noreply.github.com>
2023-11-14 21:43:09 -0500

    Update ForgotPassword.js

--- src/components/ForgotPassword/ForgotPassword.js
+++ src/components/ForgotPassword/ForgotPassword.js
@@ -21,9 +21,14 @@ const ForgotPassword = ({ className, onSubmit }) => {
 
   const handleResponse = response => {
     return new Promise((resolve, reject) => {
-      response.error !== undefined
-        ? reject(response.error)
-        : resolve(response.message)
+      if (!response) {
+        reject(response)
+      }
+      if (response.error !== undefined) { 
+        reject(response.error) 
+      } else {
+        resolve(response.message)
+      }
     })
   }
 
